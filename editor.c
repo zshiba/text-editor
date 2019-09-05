f136bdfd59dbaefbcbebe28ec30e5a43438ed68e
zshiba <zshiba@users.noreply.github.com>
2019-09-05 10:55:14 -0400

    Add Editor struct

--- editor.c
+++ editor.c
@@ -1,7 +1,18 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Editor{
+} Editor;
+
+Editor* createEditor(){
+  Editor* editor = malloc(sizeof(Editor));
+  return editor;
+}
 
 int main(){
+  Editor* editor = createEditor();
+
   bool isDone = false;
   while(!isDone){
     int c = getchar();
@@ -10,5 +21,7 @@ int main(){
     else
       isDone = true;
   }
+
+  free(editor);
   return 0;
 }
