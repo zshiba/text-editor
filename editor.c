2972f9e4f8ef70a7207fdfa530e593f3d66c7787
zshiba <zshiba@users.noreply.github.com>
2019-09-05 12:51:57 -0400

    Raw mode terminal I/O setup

--- editor.c
+++ editor.c
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 typedef struct _Editor{
 } Editor;
@@ -17,13 +20,52 @@ void start(Editor* editor){
     if(c == EOF || c == 'q')
       isDone = true;
     else
-      printf("%c\n", c);
+      printf("%c\r\n", c);
   }
 }
 
+struct termios* createRawModeTerminalIOSettinsFrom(struct termios* original){
+  struct termios* raw = malloc(sizeof(struct termios));
+  memcpy(raw, original, sizeof(struct termios));
+
+  //(input mode) BRKINT: (for SIGINT)
+  //             ICRNL: for CTRL+M
+  //             INPCK: (for input parity check)
+  //             ISTRIP: (for strip character)
+  //             IXON: for CTRL+S and CTRL+Q
+  raw->c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+  //(output mode) OPOST: to turn off post-processing features
+  raw->c_oflag &= ~(OPOST);
+
+  //(control mode) CS8: (to set CSIZE, Character size, to be 8 bits.)
+  raw->c_cflag |= (CS8);
+
+  //(local mode) ECHO: to turn off echoing
+  //             ICANON: to read inputs byte-by-byte
+  //             IEXTEN: for CTRL+V
+  //             ISIG: for CTRL+C
+  raw->c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  //(control chars) VMIN: to wait to read 1 byte
+  //                VTIME: to wait 0.X sec
+  raw->c_cc[VMIN] = 1;
+  //raw->c_cc[VTIME] = 1;
+  return raw;
+}
+
 int main(){
+  struct termios* original = malloc(sizeof(struct termios));
+  tcgetattr(STDIN_FILENO, original);
+  struct termios* raw = createRawModeTerminalIOSettinsFrom(original);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, raw);
+  free(raw);
+
   Editor* editor = createEditor();
   start(editor);
   free(editor);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, original);
+  free(original);
   return 0;
 }
